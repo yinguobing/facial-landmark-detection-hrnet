465f10587eb421f2fd2925fab18f4e098c93d28f
yinguobing <yinguobing@gmail.com>
2020-10-05 10:58:49 +0800

    custom learning rate based on epoch

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from network import HRNetV2
 from preprocess import normalize
 
 parser = ArgumentParser()
-parser.add_argument("--epochs", default=10, type=int,
+parser.add_argument("--epochs", default=60, type=int,
                     help="Number of training epochs.")
 parser.add_argument("--batch_size", default=32, type=int,
                     help="Training batch size.")
@@ -65,6 +65,45 @@ def parse_dataset(dataset):
     return parsed_dataset
 
 
+class EpochBasedLearningRateSchedule(keras.callbacks.Callback):
+    """Learning rate scheduler which sets the learning rate according to schedule.
+
+    Arguments:
+    schedule: a function that takes an epoch index (integer, indexed from 0) and
+         current learning rate as inputs and returns a new learning rate as 
+         output (float).
+  """
+
+    def __init__(self, schedule):
+        super(EpochBasedLearningRateSchedule, self).__init__()
+        self.schedule = schedule
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if not hasattr(self.model.optimizer, "lr"):
+            raise ValueError('Optimizer must have a "lr" attribute.')
+
+        # Get the current learning rate from model's optimizer.
+        lr = float(tf.keras.backend.get_value(
+            self.model.optimizer.learning_rate))
+
+        # Get the scheduled learning rate.
+        def _lr_schedule(epoch, lr, schedule):
+            """Helper function to retrieve the scheduled learning rate based on
+             epoch."""
+            if epoch < schedule[0][0] or epoch > schedule[-1][0]:
+                return lr
+            for i in range(len(schedule)):
+                if epoch == schedule[i][0]:
+                    return schedule[i][1]
+            return lr
+
+        scheduled_lr = _lr_schedule(epoch, lr, self.schedule)
+
+        # Set the value back to the optimizer before this epoch starts
+        tf.keras.backend.set_value(self.model.optimizer.lr, scheduled_lr)
+        print("\nEpoch %05d: Learning rate is %6.6f." % (epoch, scheduled_lr))
+
+
 if __name__ == "__main__":
 
     # Create the model.
@@ -110,7 +149,15 @@ if __name__ == "__main__":
                                                            histogram_freq=1024,
                                                            write_graph=True,
                                                            update_freq='epoch')
-        callbacks = [callback_checkpoint, callback_tensorboard]
+
+        # Schedule the learning rate with (epoch to start, learning rate) tuples
+        schedule = [(1, 0.0001),
+                    (30, 0.00001),
+                    (50, 0.000001)]
+        callback_lr = EpochBasedLearningRateSchedule(schedule)
+
+        # List all the callbacks.
+        callbacks = [callback_checkpoint, callback_tensorboard, callback_lr]
 
         # Construct training datasets.
         record_file_train = "/home/robin/data/facial-marks/wflw/tfrecord/wflw_train.record"

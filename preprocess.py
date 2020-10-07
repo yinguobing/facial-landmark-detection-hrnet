c4b06a1b4ef9c74ab17951f1bd2f51a15c829da6
yinguobing <yinguobing@gmail.com>
2020-10-07 16:22:28 +0800

    add function to rotate the image

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,5 @@
+"""This module provides commonly used image preprocessing functions."""
+import cv2
 import tensorflow as tf
 
 
@@ -13,3 +15,29 @@ def normalize(inputs):
 
     # Normalization
     return ((inputs / 255.0) - img_mean)/img_std
+
+
+def rotate(image, degrees):
+    """Rotate the image in degrees.
+
+    Args:
+        image: an image with face to be processed.
+        degrees: degrees to rotate. 
+
+    Returns:
+        a same size image rotated.
+    """
+    img_height, img_width, _ = image.shape
+    rotation_mat = cv2.getRotationMatrix2D(((img_width-1)/2.0,
+                                            (img_height-1)/2.0), degrees, 1)
+    image_rotated = cv2.warpAffine(
+        image, rotation_mat, (img_width, img_height))
+
+    return image_rotated
+
+
+if __name__ == "__main__":
+    img = cv2.imread("/home/robin/Desktop/sample/face.jpg")
+    img_rotated = rotate(img, -30)
+    cv2.imshow("rotate", img_rotated)
+    cv2.waitKey()

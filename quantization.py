5f80aa15b741b66bf1bcfa572f152c99ea5064b0
yinguobing <yinguobing@gmail.com>
2020-10-16 15:06:31 +0800

    fix init function for TFLiteModelPredictor

--- quantization.py
+++ quantization.py
@@ -85,8 +85,8 @@ class TFLiteModelPredictor(object):
     def __init__(self, model_path):
         self.interpreter = tf.lite.Interpreter(model_path)
         self.interpreter.allocate_tensors()
-        self.input_index = interpreter.get_input_details()[0]["index"]
-        self.output_index = interpreter.get_output_details()[0]["index"]
+        self.input_index = self.interpreter.get_input_details()[0]["index"]
+        self.output_index = self.interpreter.get_output_details()[0]["index"]
 
     def predict(self, inputs):
         self.interpreter.set_tensor(self.input_index, inputs)

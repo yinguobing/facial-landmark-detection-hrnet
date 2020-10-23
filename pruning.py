e42d7f33a285bc6a0f7fe41676d87224cf2afca7
yinguobing <yinguobing@gmail.com>
2020-10-23 11:41:12 +0800

    add module for model pruning

--- pruning.py
+++ pruning.py
@@ -1,3 +1,4 @@
+"""Optimize the model with pruning."""
 import os
 from argparse import ArgumentParser
 
@@ -5,8 +6,8 @@ import tensorflow as tf
 import tensorflow_model_optimization as tfmot
 from tensorflow import keras
 
-from dataset import make_wflw_dataset
-from network import HRNetV2
+from dataset import build_dataset_from_wflw
+from network import hrnet_v2
 
 
 parser = ArgumentParser()
@@ -20,25 +21,26 @@ args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    # Create the model.
-    model = HRNetV2(width=18, output_channels=98)
+    # There are 3 steps for model pruning.
+    #   1. Load the model with pretrained weights.
+    #   2. Prune the model during training.
+    #   3. Export the model.
 
-    # Restore the latest model if checkpoints are available.`
+    # Where are the pretrained weights.
     checkpoint_dir = "./checkpoints"
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
-        print("Checkpoint directory created: {}".format(checkpoint_dir))
 
+    # Where the pruned model will be exported
+    pruned_model_path = "./optimized/pruned"
+
+    # First, create the model and restore it with pretrained weights.
+    model = hrnet_v2((256, 256, 3), width=18, output_channels=98)
+
+    # Restore the latest model from checkpoint.
     latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
-    if latest_checkpoint:
-        model.load_weights(latest_checkpoint)
-        print("Checkpoint restored: {}".format(latest_checkpoint))
+    model.load_weights(latest_checkpoint)
+    print("Checkpoint restored: {}".format(latest_checkpoint))
 
-    model.compile(optimizer=keras.optimizers.Adam(0.0001),
-                  loss=keras.losses.MeanSquaredError(),
-                  metrics=[keras.metrics.MeanSquaredError()])
-
-    # Setup the pruning.
+    # Second, Setup the pruning.
     pruning_params = {
         'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(
             initial_sparsity=0.5,
@@ -50,60 +52,45 @@ if __name__ == "__main__":
     model_pruned = tfmot.sparsity.keras.prune_low_magnitude(
         model, **pruning_params)
 
-    model_pruned.summary()
-
     # Hyper parameters for training.
     epochs = args.epochs
     batch_size = args.batch_size
 
-    # Save a checkpoint. This could be used to resume training.
-    checkpoint_path = os.path.join(checkpoint_dir, "ckpt")
-    callback_checkpoint = keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path,
-        save_weights_only=False,
-        verbose=1,
-        save_best_only=True)
-
     callbacks = [
         tfmot.sparsity.keras.UpdatePruningStep(),
-        tfmot.sparsity.keras.PruningSummaries(log_dir="./log"),
+        tfmot.sparsity.keras.PruningSummaries(log_dir="./logs"),
     ]
 
     # Construct training datasets.
     train_files_dir = "/home/robin/data/facial-marks/wflw_cropped/train"
-    dataset_train = make_wflw_dataset(train_files_dir, "wflw_train",
-                                      training=True,
-                                      batch_size=args.batch_size,
-                                      mode="generator")
-    if not isinstance(dataset_train, keras.utils.Sequence):
-        dataset_train = dataset_train.batch(args.batch_size)
+    dataset_train = build_dataset_from_wflw(train_files_dir, "wflw_train",
+                                            training=True,
+                                            batch_size=batch_size,
+                                            shuffle=True,
+                                            prefetch=tf.data.experimental.AUTOTUNE,
+                                            mode="generator")
 
     # Construct dataset for validation & testing.
     test_files_dir = "/home/robin/data/facial-marks/wflw_cropped/test"
-    dataset_val = make_wflw_dataset(test_files_dir, "wflw_test",
-                                    training=False,
-                                    batch_size=args.batch_size,
-                                    mode="generator")
-    if not isinstance(dataset_val, keras.utils.Sequence):
-        dataset_val = dataset_val.batch(args.batch_size)
+    dataset_val = build_dataset_from_wflw(test_files_dir, "wflw_test",
+                                          training=False,
+                                          batch_size=batch_size,
+                                          shuffle=False,
+                                          prefetch=tf.data.experimental.AUTOTUNE,
+                                          mode="generator")
 
+    # Compile the model for pruning.
     model_pruned.compile(optimizer=keras.optimizers.Adam(0.0001),
                          loss=keras.losses.MeanSquaredError(),
                          metrics=[keras.metrics.MeanSquaredError()])
+    model_pruned.summary()
 
     # Start training loop.
     model_pruned.fit(dataset_train, validation_data=dataset_val,
                      epochs=epochs, callbacks=callbacks,
                      initial_epoch=args.initial_epoch)
 
-    model_pruned.summary()
-
-    # Export
+    # At last, Export the pruned model.
     model_for_export = tfmot.sparsity.keras.strip_pruning(model_pruned)
-
-    converter = tf.lite.TFLiteConverter.from_keras_model(model_for_export)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    pruned_tflite_model = converter.convert()
-
-    with open("pruned_tflite_file.tflite", 'wb') as f:
-        f.write(pruned_tflite_model)
+    model_for_export.save(pruned_model_path, include_optimizer=False)
+    print("Pruned model saved to: {}".format(pruned_model_path))

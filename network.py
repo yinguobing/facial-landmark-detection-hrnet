30a051e6494f8a7d6a72851f8c57b62671d9f451
yinguobing <yinguobing@gmail.com>
2020-12-11 18:04:22 +0800

    support naming the model by user code

--- network.py
+++ network.py
@@ -128,7 +128,7 @@ class HRNetHeads(layers.Layer):
         return prunable_weights
 
 
-def hrnet_v2(input_shape=(256, 256, 3), width=18, output_channels=98):
+def hrnet_v2(input_shape, output_channels, width=18, name="hrnetv2"):
     """This function returns a functional model of HRNetV2.
 
     Args:
@@ -149,7 +149,7 @@ def hrnet_v2(input_shape=(256, 256, 3), width=18, output_channels=98):
                           output_channels=output_channels)(x)
 
     # Construct the model and return it.
-    model = keras.Model(inputs=inputs, outputs=outputs, name="hrnetv2")
+    model = keras.Model(inputs=inputs, outputs=outputs, name=name)
 
     return model
 
